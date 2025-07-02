e8ceb775810c24b9986219895c6b6bfe6425eba8
Sihuan Newrise <sihuannewrise@gmail.com>
2025-07-02 13:39:43 +0300

    test parser

--- chanks/whats_new.py
+++ chanks/whats_new.py
@@ -1,10 +1,40 @@
 import requests_cache
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from tqdm import tqdm
 
 WHATS_NEW_URL = 'https://docs.python.org/3/whatsnew/'
 
 if __name__ == '__main__':
-    # Загрузка веб-страницы с кешированием.
     session = requests_cache.CachedSession()
     response = session.get(WHATS_NEW_URL)
+    response.encoding = 'utf-8'
 
-    print(response.text)
+    soup = BeautifulSoup(response.text, features='lxml')
+    main_div = soup.find('section', attrs={'id': 'what-s-new-in-python'})
+    div_with_ul = main_div.find('div', attrs={'class': 'toctree-wrapper'})
+    sections_by_python = div_with_ul.find_all('li', attrs={'class': 'toctree-l1'})
+
+    # for section in sections_by_python:
+    #     version_a_tag = section.find('a')
+    #     href = version_a_tag['href']
+    #     version_link = urljoin(WHATS_NEW_URL, href)
+    #     print(version_link)
+
+    results = []
+    for section in tqdm(sections_by_python):
+        version_a_tag = section.find('a')
+        href = version_a_tag['href']
+        version_link = urljoin(WHATS_NEW_URL, href)
+
+        response = session.get(version_link)
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup(response.text, features='lxml')
+        h1 = soup.find('h1')
+        h1_text = h1.text[:-1]
+        dl = soup.find('dl')
+        dl_text = dl.text.replace('\n', ' ')
+        results.append((version_link, h1_text, dl_text))
+
+    for row in results:
+        print(*row)
